f97fae55dd16aac1cf8dc16e2676feac0ce8531c
timtikh <95489979+timtikh@users.noreply.github.com>
2023-12-04 19:37:48 +0300

    changed everything

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="App">
+      <h1>React Form with Preview</h1>
+      <form>
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+      </form>
 
+      <div className="preview">
+        <h2>Preview:</h2>
+        <p>
+          Name: {formData.firstName} {formData.lastName}
+        </p>
+        <p>Email: {formData.email}</p>
+      </div>
     </div>
   );
 }
